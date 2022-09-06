8e29764c926845cafac28f7c88bfb36a472558b2
Mandy Mason <mnmason86@gmail.com>
2022-09-06 18:58:58 -0400

    initial commit

---
+++ src/main/java/com/mnmason6/codefellowship/CodeFellowshipApplication.java
@@ -0,0 +1,13 @@
+package com.mnmason6.codefellowship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeFellowshipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeFellowshipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mnmason6/codefellowship/CodeFellowshipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mnmason6.codefellowship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeFellowshipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
